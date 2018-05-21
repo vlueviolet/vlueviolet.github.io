5b9950d438e4cd2345416b396ce8e6c08f29b04c
Dajung <vlueviolet@gmail.com>
2018-05-21 18:46:47 +0900

    top 버튼 추가

--- js/uio/top_button/js/apps/ui.js
+++ js/uio/top_button/js/apps/ui.js
@@ -10,7 +10,6 @@
             },
             setElements : function () {
                 this.topBtn = $('.btn_top');
-                this.minOffsetTop = $('#wrap').offset().top;    // 특정 element의 offset 값으로 하면 레이어 팝업 scroll 제어에 편함
             },
             bindEvents : function () {
                 $(win).on('scroll', $.proxy(this.scrollFunc, this));
@@ -18,7 +17,8 @@
             },
             scrollFunc : function () {
                 var winTop = $(win).scrollTop();
-                if(winTop > 0) {
+                this.minOffsetTop = $('#wrap').offset().top;
+                if(winTop > this.minOffsetTop) {
                     if(!this.topBtn.is(':visible')) {
                         this.topBtn.stop(true, true).fadeIn('fast');
                     }
