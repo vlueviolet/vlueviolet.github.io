5760e76a71f4ffd9bc7b4a1e61aad5897635f395
최다정 <viva_dj@hivelab.co.kr>
2019-07-09 00:27:09 +0900

    commit

--- study/hivelab/code_squad/newsui/js/main.js
+++ study/hivelab/code_squad/newsui/js/main.js
@@ -1,9 +1,10 @@
 import newsSection from './newsSection.js'
 import {fnNewsListTemplate} from '../templates/news.js'
+import {fnNewsTitleTemplate} from '../templates/newsTitle.js'
 
 // dom tree가 만들어졌을떄, 즉 javascript에 접근할 수 있을 떄
 window.addEventListener("DOMContentLoaded", () => {
     const url = "/data/newslist.json";
     const news = new newsSection(url);
-    news.init(fnNewsListTemplate);
+    news.init(fnNewsListTemplate, fnNewsTitleTemplate);
 });
--- study/hivelab/code_squad/newsui/js/newsSection.js
+++ study/hivelab/code_squad/newsui/js/newsSection.js
@@ -5,18 +5,24 @@ export default class newsSection {
         this.preveIndex = 0;
     }
 
-    init(fnNewsListTemplate) {
+    init(fnNewsListTemplate, fnNewsTitleTemplate) {
         // 서버로 보내서 fetch요청을 해야함
-        this.fetchData(fnNewsListTemplate);
+        this.fetchData(fnNewsListTemplate, fnNewsTitleTemplate);
     }
 
-    fetchData(fnNewsListTemplate) {
+    fetchData(fnNewsListTemplate, fnNewsTitleTemplate) {
+        // console.log(fnNewsTitleTemplate)
+        // return;
         const textArray = [];
+        const textArray2 = [];
         fetch(this.fetchUrl)
             .then(res => res.json())
             .then(data => {
                 data.forEach(array => textArray.push(fnNewsListTemplate(array)));
                 document.querySelector('.content').innerHTML = textArray.join('');
+                
+                data.forEach(array => textArray2.push(fnNewsTitleTemplate(array)));
+                document.querySelector('.newsNavigation').innerHTML = textArray2.join('');
             })
             .then(_ => {
                 this.setElements();
@@ -28,11 +34,13 @@ export default class newsSection {
         this.btnLeft = document.querySelector('.btn .left a');
         this.btnRight = document.querySelector('.btn .right a');
         this.slide = Array.from(document.querySelectorAll('.news_item'));
+        this.navigationList = Array.from(document.querySelectorAll('.newsNavigation li'));
     }
 
     bindEvents() {
         this.btnLeft.addEventListener('click', () => this.moveSlide('left'));
         this.btnRight.addEventListener('click', () => this.moveSlide('right'));
+        this.navigationList.forEach(array => array.addEventListener('click', () => this.clickTitle(event)));
     }
 
     moveSlide(direction) {
@@ -54,6 +62,8 @@ export default class newsSection {
         }
         this.slide[this.preveIndex].style.display = 'none';
         this.slide[this.currentIndex].style.display = 'block';
+        this.navigationList[this.preveIndex].classList.remove('is_active');
+        this.navigationList[this.currentIndex].classList.add('is_active');
 
         // this.slide.forEach((array, index) => {
         //     if (index === this.currentIndex) {
@@ -63,5 +73,10 @@ export default class newsSection {
         //     }
         // })
     }
-
+    clickTitle({target}) {
+        if (target.parentNode.classList.contains('is_active')) return;
+        this.preveIndex = this.currentIndex;
+        console.log(target.index())
+        target.parentNode.classList.add('is_active');
+    }
 }
