33f4ebb7beab7d2cb6934cd8147c401671d43bcb
최다정 <viva_dj@hivelab.co.kr>
2019-09-27 01:00:46 +0900

    add

--- study/react/live-cryptocurrency-react/src/App.js
+++ study/react/live-cryptocurrency-react/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import Cryptocurrency from './component/Cryptocurrency'
 
-// class App extends Component {
-//     render() {
-//         return (
-//             <Cryptocurrency />
-//         );
-//     }
-// }
-const App = ({}) => pug`
-      h1 Hello
-`;
+class App extends Component {
+    render() {
+        return (
+            <Cryptocurrency />
+        );
+    }
+}
 
 export default App;
 
