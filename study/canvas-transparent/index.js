a4714840d8c021301b24d41eb849219e73467c59
dajung.choi <viva_dj@hivelab.co.kr>
2020-06-12 12:51:47 +0900

    add canvas

--- study/canvas-transparent/index.js
+++ study/canvas-transparent/index.js
@@ -30,7 +30,8 @@ let processor = {
       let r = frame.data[i * 4 + 0];
       let g = frame.data[i * 4 + 1];
       let b = frame.data[i * 4 + 2];
-      if(g > 100 && r < 100) frame.data[i * 4 + 3] = 0;
+      // if(g > 100 && r < 100) frame.data[i * 4 + 3] = 0;
+      frame.data[i * 4 + 3] = 1;
     }
     this.ctx2.putImageData(frame, 0, 0);
   }
