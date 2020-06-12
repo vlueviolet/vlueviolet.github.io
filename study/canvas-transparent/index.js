e434d57813c90a6afb322f96ee18ce9180482227
dajung.choi <viva_dj@hivelab.co.kr>
2020-06-12 13:25:55 +0900

    ie 대응

--- study/canvas-transparent/index.js
+++ study/canvas-transparent/index.js
@@ -1,14 +1,15 @@
-let processor = {
+var processor = {
   timerCallback: function() {
     if(this.video.paused || this.video.ended) return;
 
     this.computeFrame();
-    let self = this;
-    setTimeout(() => {
-      this.timerCallback();
+    var self = this;
+    setTimeout(function() {
+      self.timerCallback();
     });
   },
   doLoad: function() {
+    var self = this;
     this.video = document.querySelector('#video');
     this.normal = document.querySelector('#normal');
     this.narmalContext = this.normal.getContext('2d');
@@ -23,11 +24,11 @@ let processor = {
     //     width = outputCanvas.width,
     //     height = outputCanvas.height,
     //     interval;
-    
-    this.video.addEventListener('play', () => {
-      this.width = 150;
-      this.height = 300;
-      this.timerCallback();
+
+    this.video.addEventListener('play', function() {
+      self.width = 150;
+      self.height = 300;
+      self.timerCallback();
     });
   },
   computeFrame: function() {
@@ -37,13 +38,13 @@ let processor = {
         imageData = image.data,
         alphaData = this.narmalContext.getImageData(0, transparent.height, transparent.width, transparent.height).data;
 
-    let frame = this.narmalContext.getImageData(0, 0, this.width, this.height);
-    let max = frame.data.length / 4;
+    var frame = this.narmalContext.getImageData(0, 0, this.width, this.height);
+    var max = frame.data.length / 4;
 
-    for(let i=0 ; i<max ; i++) {
-      let r = frame.data[i * 4 + 0];
-      let g = frame.data[i * 4 + 1];
-      let b = frame.data[i * 4 + 2];
+    for(var i=0 ; i<max ; i++) {
+      var r = frame.data[i * 4 + 0];
+      var g = frame.data[i * 4 + 1];
+      var b = frame.data[i * 4 + 2];
       if(g > 100 && r < 100) frame.data[i * 4 + 3] = 0;
     }
     for (let i = 3, len = imageData.length; i < len; i = i + 4) {
@@ -51,10 +52,10 @@ let processor = {
     }
 
     // green, red 처리
-    // this.transparentContext.putImageData(frame, 0, 0);
-    
+    this.transparentContext.putImageData(frame, 0, 0);
+
     // 전체 opacity
-    this.transparentContext.putImageData(image, 0, 0);
+    // this.transparentContext.putImageData(image, 0, 0);
   }
 };
 
