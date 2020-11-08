fcb4f07f1dc294750f96b08c547ea9b23b81b180
최다정 <viva_dj@hivelab.co.kr>
2020-11-08 09:25:39 +0900

    :hammer: add icon import case

--- study/storybook/src/stories/Icon/Icon.tsx
+++ study/storybook/src/stories/Icon/Icon.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import IconCloud from 'asset/images/svg/icon-cloud.svg';
+const reqSvgs = require.context('asset/images/svg', true, /\.svg$/);
 
 export interface IconProps {
   /** svg element */
@@ -12,7 +13,24 @@ export interface IconProps {
 }
 
 const Icon = ({ IconSvgElement, size, color }: IconProps) => {
-  return <IconSvgElement width={size} height={size} color={color} />;
+  const test = document.getElementById('test');
+  console.log(test);
+
+  return (
+    <>
+      {/* <IconSvgElement width={size} height={size} color={color} /> */}
+      1111
+      <div>
+        {reqSvgs.keys().map((filename) => {
+          import(`asset/images/svg/${filename.replace('./', '')}`)
+            // .then((module) => module.loadPageInto(test))
+            .then((module) => module.loadPageInto(test))
+            .catch(() => console.error('실패'));
+        })}
+      </div>
+      2222
+    </>
+  );
 };
 
 Icon.defaultProps = {
